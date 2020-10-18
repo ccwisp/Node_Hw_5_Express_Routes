69d412d1a0e866dfe9c024e54e03dafa392554c2
ccwisp <armnecro@gmail.com>
2020-10-18 23:05:34 +0400

    initial commit

---
+++ bin/www.js
@@ -0,0 +1,25 @@
+/**
+ * Module dependencies.
+ */
+
+const app = require('../app');
+const http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
